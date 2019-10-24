dde1e69b7a2790007de9ff9a1246320d7e70f446
unknown <shuklaanupam18@gmail.com>
2019-10-25 00:13:08 +0530

    Exporting data to csv

--- js/main.js
+++ js/main.js
@@ -1,43 +1,29 @@
-window.onload = function() {
+window.onload = function () {
 
     var x = [];
 
     //start the webgazer tracker
     webgazer.setRegression('ridge') /* currently must set regression and tracker */
         .setTracker('clmtrackr')
-        .setGazeListener(function(data, clock) {
+        .setGazeListener(function (data, clock) {
             // console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-            
-            if(data!=null){
-            var predx = data["x"];
-            var predy = data["y"];
-            x.push([predx, predy]);
 
-            console.log(data["x"]+","+data["y"]);
+            if (data != null) {
+                var predx = data["x"];
+                var predy = data["y"];
+                x.push([predx, predy]);
+
+                console.log(data["x"] + "," + data["y"]);
             }
-            
+
             //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         })
         .begin()
         .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
 
-    // exporting data to .csv
-    function saveGaze() {
-        var csv = '';
-        x.forEach(function (row) {
-            csv += row.join(',');
-            csv += "\n";
-        });
-
-        var hiddenElement = document.createElement('a');
-        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
-        hiddenElement.target = '_blank';
-        hiddenElement.download = 'gazeData.csv';
-        hiddenElement.click();
-    }
 
     //Set up the webgazer video feedback.
-    var setup = function() {
+    var setup = function () {
 
         //Set up the main canvas. The main canvas is used to calibrate the webgazer.
         var canvas = document.getElementById("plotting_canvas");
@@ -53,10 +39,10 @@ window.onload = function() {
             setTimeout(checkIfReady, 100);
         }
     }
-    setTimeout(checkIfReady,100);
+    setTimeout(checkIfReady, 100);
 };
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     //webgazer.end(); //Uncomment if you want to save the data even if you reload the page.
     window.localStorage.clear(); //Comment out if you want to save data across different sessions
 }
@@ -64,8 +50,24 @@ window.onbeforeunload = function() {
 /**
  * Restart the calibration process by clearing the local storage and reseting the calibration point
  */
-function Restart(){
+function Restart() {
     document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
     ClearCalibration();
     PopUpInstruction();
+}
+
+
+//  exporting data to .csv
+function saveGaze() {
+    var csv = '';
+    x.forEach(function (row) {
+        csv += row.join(',');
+        csv += "\n";
+    });
+
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'gazeData.csv';
+    hiddenElement.click();
 }
