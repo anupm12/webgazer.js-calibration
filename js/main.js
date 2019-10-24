337b0b9e97c966515a74fb5d9b133d4efb44ae5e
unknown <shuklaanupam18@gmail.com>
2019-10-24 15:46:41 +0530

    Log predections

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@ window.onload = function() {
     webgazer.setRegression('ridge') /* currently must set regression and tracker */
         .setTracker('clmtrackr')
         .setGazeListener(function(data, clock) {
-          //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-        //   console.log(data[x]);
-        //   console.log(data[y]);
+            console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
           //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         })
         .begin()
